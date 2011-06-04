81e4c6bf84f4ca600e2bfe05668b077be07c9259
bala.dutt@gmail.com <bala.dutt@gmail.com@f9432db4-ed33-104d-b472-94fa44182c47>
2011-06-04 10:54:22 +0000

    map is visible, now

--- src/com/bhago/Main.java
+++ src/com/bhago/Main.java
@@ -1,13 +1,23 @@
 package com.bhago;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class Main extends Activity {
-    /** Called when the activity is first created. */
+    @Override
+	public boolean onTouchEvent(MotionEvent event) {
+		Intent intent = new Intent(this, SafeRouteActivity.class);
+		startActivity(intent);
+		return true;
+	}
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
 }

1571548614c4260c20272c26f904c6e371e884d5
jayanthsaimani@gmail.com <jayanthsaimani@gmail.com@f9432db4-ed33-104d-b472-94fa44182c47>
2011-06-04 09:56:01 +0000

    Map

---
+++ src/com/bhago/SafeRouteActivity.java
@@ -0,0 +1,43 @@
+package com.bhago;
+
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+public class SafeRouteActivity extends MapActivity {
+	
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+	    return false;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.mapview);
+	    
+	    MapView mapView = (MapView) findViewById(R.id.mapView);
+	    mapView.setBuiltInZoomControls(true);
+	    
+	    List<Overlay> mapOverlays = mapView.getOverlays();
+	    Drawable drawable = this.getResources().getDrawable(R.drawable.icon);
+	    WaterOverlay itemizedoverlay = new WaterOverlay(drawable);
+	    
+	    GeoPoint point = new GeoPoint(19240000,-99120000);
+	    OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+	    
+	    itemizedoverlay.addOverlay(overlayitem);
+	    mapOverlays.add(itemizedoverlay);
+	    
+	}
+	
+
+}

d84d2ac048cea1682df7f01cd8bba33edf713459
bala.dutt@gmail.com <bala.dutt@gmail.com@f9432db4-ed33-104d-b472-94fa44182c47>
2011-06-05 04:02:59 +0000

    able to show nearby-locations with home icon

--- src/com/bhago/gamemodel/MyLocation.java
+++ src/com/bhago/gamemodel/MyLocation.java
@@ -7,11 +7,17 @@ public class MyLocation{
 	public double latitude; //ofcourse earth's
 	public double longitude;
 	public double elevation;
+	public String name = null;
 	
 	public MyLocation(double latitude, double longitude)
 	{
 		this.latitude = latitude;
 		this.longitude =longitude;
+	}
+
+	public MyLocation(String name, float lat, float lng) {
+		this(lat, lng);
+		this.name = name;
 	}
 
 }
